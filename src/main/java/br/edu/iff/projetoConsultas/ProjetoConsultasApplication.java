2789a2da2b4f8d0e0d0754ecb1b59db29d90e99e
Carol <Carol@DESKTOP-IP7JERB>
2022-03-06 15:11:31 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/iff/projetoConsultas/ProjetoConsultasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoConsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConsultasApplication.class, args);
+	}
+
+}
